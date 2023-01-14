dd3a0cb10d60ba6d4e69fff2309de4b3e3152ce8
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2023-01-14 10:24:59 +0200

    169-mod activities

--- app/src/main/java/com/piy/happyplaces/app/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/piy/happyplaces/app/AddHappyPlaceActivity.kt
@@ -2,10 +2,18 @@ package com.piy.happyplaces.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
+        setSupportActionBar(toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toolbarAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
