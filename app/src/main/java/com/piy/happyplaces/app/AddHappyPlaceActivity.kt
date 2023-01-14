78a2848b858e5c3361af786559409571b8415500
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2023-01-14 17:39:51 +0200

    170 - add the date picker

--- app/src/main/java/com/piy/happyplaces/app/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/piy/happyplaces/app/AddHappyPlaceActivity.kt
@@ -1,10 +1,17 @@
 package com.piy.happyplaces.app
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+    private val calendar = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
@@ -15,5 +22,35 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         toolbarAddPlace.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+        etDate.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.etDate -> {
+                println("CLICKING ON DATE PICKER!!!")
+                DatePickerDialog(
+                    this@AddHappyPlaceActivity,
+                    dateSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH),
+                ).show()
+            }
+        }
+    }
+
+    private fun updateDateInView (){
+        val formatter = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(formatter, Locale.getDefault())
+        etDate.setText(sdf.format(calendar.time).toString())
     }
 }
