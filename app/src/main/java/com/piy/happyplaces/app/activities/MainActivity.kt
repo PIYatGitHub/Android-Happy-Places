dda9717537f66b7b1903b08293c6ae2ab6635340
Peter Y.(sewBoT) <petyr.yonkov@mail.bg>
2023-01-28 17:43:53 +0200

    180 - add recycler view in MainActivity.kt

--- app/src/main/java/com/piy/happyplaces/app/activities/MainActivity.kt
+++ app/src/main/java/com/piy/happyplaces/app/activities/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.piy.happyplaces.app.R
+import com.piy.happyplaces.app.adapters.HappyPlacesAdapter
 import com.piy.happyplaces.app.database.DatabaseHandler
 import com.piy.happyplaces.app.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,15 +23,24 @@ class MainActivity : AppCompatActivity() {
         getHappyPlacesFromLocalDB()
     }
 
-    private fun getHappyPlacesFromLocalDB(){
+    private fun getHappyPlacesFromLocalDB() {
         val db = DatabaseHandler(this)
         val happyPlaceList: ArrayList<HappyPlaceModel> = db.getHappyPlacesList()
 
-        if(happyPlaceList.size>0){
-            for (i in happyPlaceList){
-                Log.e("[ENTRIES - TITLE]", i.title)
-                Log.e("[ENTRIES - DESCRIPTION]", i.description)
-            }
+        if (happyPlaceList.size > 0) {
+            rvHappyPlacesList.visibility = View.VISIBLE
+            tvNoItems.visibility = View.GONE
+            setupHappyPlacesRecyclerView(happyPlaceList)
+        } else{
+            rvHappyPlacesList.visibility = View.GONE
+            tvNoItems.visibility = View.VISIBLE
         }
+    }
+
+    private fun setupHappyPlacesRecyclerView(happyPlaceList: ArrayList<HappyPlaceModel>) {
+        rvHappyPlacesList.layoutManager = LinearLayoutManager(this)
+        rvHappyPlacesList.setHasFixedSize(true)
+        val placesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+        rvHappyPlacesList.adapter = placesAdapter
     }
 }
